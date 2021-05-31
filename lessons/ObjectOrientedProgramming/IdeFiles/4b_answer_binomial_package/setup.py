d0465dffd7d898a847642f9ea9020e01bd0bac20
lisabanh <84567442+lisabanh@users.noreply.github.com>
2021-05-31 20:00:30 +0100

    Update setup.py

--- lessons/ObjectOrientedProgramming/IdeFiles/4b_answer_binomial_package/setup.py
+++ lessons/ObjectOrientedProgramming/IdeFiles/4b_answer_binomial_package/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='distributions',
       version='0.1',
-      description='Gaussian distributions',
+      description='Gaussian & Binomial distributions',
       packages=['distributions'],
       zip_safe=False)
